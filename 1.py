2bb696a91db18ac00545694986ed711fa7bc9146
LUAN LIMA FERREIRA <ferreira.luan@aluno.senac.rio>
2025-09-24 19:08:28 -0300

    update aula3

--- 1.py
+++ 1.py
@@ -1,23 +1,35 @@
-class carro:
-    rodas = 4
-    
-    
-def __init__ (self, marca, ano):
-    self.marca = marca
-    self.ano = ano
+class Carro:
+    rodas = 4  # Atributo de classe
+
+    def __init__(self, marca, ano):
+        self.marca = marca
+        self.ano = ano
+
+    def ligar(self):
+        print(f'O carro {self.marca} ({self.ano}) está ligado')
+
+    @classmethod
+    def mudar_numero_rodas(cls, novas_rodas):
+        cls.rodas = novas_rodas
+        print(f'Agora todos os carros têm {cls.rodas} rodas')
+
+    @staticmethod
+    def calcular_idade(ano_fabricacao, ano_atual):
+        return ano_atual - ano_fabricacao
     
 
-def ligar(self):
-    print(f'O carro{self.marca}({self.ano}) está ligado')
-    
+
+marca = input("Digite a marca do carro: ")
+ano = int(input("Digite o ano do carro: "))
+
+meu_carro = Carro( marca, ano)
+
+meu_carro.ligar()
 
 
-@classmethod
-def mudar_numero_rodas(cls, novas_rodas):
-    cls.rodas = novas_rodas
-    print(f'Agora todos os carros tem {cls.rodas} rodas')
-    
-    
-@staticmethod
-def calcular_idade(ano_fabricacao, ano_atual):
-    return ano_atual - ano_fabricacao
+ano_atual = 2025
+
+idade = Carro.calcular_idade(meu_carro)
+print(f'O carro tem {idade} anos.')
+
+
